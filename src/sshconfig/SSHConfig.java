67466faa788e577f7dc42dfe373548b73aacbc51
Saksham Ghimire <saksham.ghimire@echostar.com>
2016-07-21 20:14:44 -0600

    SSHConfig.java: Added logic to parse Host sections from the config file.

--- src/sshconfig/SSHConfig.java
+++ src/sshconfig/SSHConfig.java
@@ -1,32 +1,34 @@
 package sshconfig;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- *
  * @author saksham.ghimire
  */
 public class SSHConfig {
-    
+
     final String userHome = System.getProperty("user.home");
     final String sshDir = ".ssh";
     final String configFile = "config";
     final String sshConfigFile_ = new File(userHome, sshDir).toString();
     final String sshConfigFile = new File(sshConfigFile_, configFile).toString();
 
-    public void print(Object o){
+    public void print(Object o) {
         System.out.println(o.toString());
     }
+
     public void checkSSHConfig() throws IOException {
 
         System.out.println(sshConfigFile);
@@ -38,39 +40,98 @@ public class SSHConfig {
             PrintWriter writer = new PrintWriter(sshFile, "UTF-8");
             writer.println("# File update by SSHConfig");
             writer.close();
-
         }
-
     }
-    
-    public void readFileIntoList() throws IOException{
-        FileReader reader = null;
+
+    public String[] readFileIntoList() throws IOException {
+        Path filePath = null;
         try {
-            reader = new FileReader(new File(sshConfigFile));
-        } catch (FileNotFoundException ex) {
+            filePath = new File(sshConfigFile).toPath();
+        } catch (Exception ex) {
             Logger.getLogger(SSHConfig.class.getName()).log(Level.SEVERE, null, ex);
         }
-        BufferedReader fileReader = new BufferedReader(reader);
-        List<String> whiteBoard = new ArrayList<String>();
-        while (fileReader.readLine()!= null){
-//            System.out.println(fileReader.readLine());
-            whiteBoard.add(fileReader.readLine());
-            
-            
-        }
-        print(whiteBoard);
+        Charset charset = Charset.defaultCharset();
+        List<String> stringList = Files.readAllLines(filePath, charset);
+        String[] stringArray = stringList.toArray(new String[]{});
+        return stringArray;
     }
-    
-    public void loadFileFromListIntoAMap(){
-        
+
+    public void loadFileFromListIntoAMap(List<String> contents) {
+
+    }
+
+    public String getHostName(String str) {
+        String[] hostNameLine = str.split("/n");
+        if (!hostNameLine[0].trim().startsWith("#")) {
+            String[] hostNameToSplit = hostNameLine[0].trim().split(" ");
+            return hostNameToSplit[1].trim();
+        } else {
+            return null;
+        }
+
+    }
+
+    public Map<String, Object> getHostProperties(String str) {
+        Map<String, Object> properties = new HashMap<>();
+        String[] lines = str.split("\n");
+
+        for (int i = 1; i < lines.length; i++) {
+            String[] fields = lines[i].trim().split(" ");
+
+            String fieldName = "";
+            if (fields.length != 0) {
+                fieldName = fields[0].trim();
+            }
+            switch (fieldName.toLowerCase()) {
+                case "hostname":
+                    properties.put("HostName", fields[1].trim());
+                case "port":
+                    properties.put("Port", fields[1].trim());
+                case "user":
+                    properties.put("User", fields[1].trim());
+                case "localforward":
+                    properties.put(fields[1].trim(), new PortForwarding("LocalForward", fields[1].trim(),fields[1].trim()));
+                    System.out.println(new PortForwarding("LocalForward", fields[1].trim(),fields[1].trim()).toString());
+            }
+        }
+        return properties;
+    }
+
+    public class PortForwarding {
+        private final String forwardType;
+        private final String localPort;
+        private final String destinationAddress;
+
+        public PortForwarding(String forwardType, String localPort, String destinationAddress) {
+            this.forwardType = forwardType;
+            this.localPort = localPort;
+            this.destinationAddress = destinationAddress;
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         SSHConfig starter = new SSHConfig();
         starter.checkSSHConfig();
-        starter.readFileIntoList();
-        starter.loadFileFromListIntoAMap();
+        String[] contents = starter.readFileIntoList();
+        String fullString = "";
+        for (String str : contents) {
+            fullString += str + "\n";
+        }
+
+        String pattern = "(^(?!\\s).*?\\n(?:\\s.*?(?:\\n|$))*)";
+        Pattern r = Pattern.compile(pattern, Pattern.MULTILINE);
+        Matcher m = r.matcher(fullString);
+
+        Map<String, Map<String, Object>> hostWithSections = new HashMap<>();
+        while (m.find()) {
+            if (starter.getHostName(m.group(0)) != null) {
+                hostWithSections.put(starter.getHostName(m.group(0)), starter.getHostProperties(m.group(0)));
+            }
+        }
+        starter.print(hostWithSections);
     }
 
 }
+
