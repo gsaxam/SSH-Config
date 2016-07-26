1f2e512e77b79722eb79f954754b02b9db5db876
Saksham Ghimire <saksham.ghimire@echostar.com>
2016-07-26 01:03:42 -0600

    Working list to side-by-side box view

--- src/SSHConfig/Main.java
+++ src/SSHConfig/Main.java
@@ -1,6 +1,8 @@
 package SSHConfig;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,25 +21,26 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import jdk.management.cmm.SystemResourcePressureMXBean;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main extends Application {
-    public static void print(Object o){
+    public static void print(Object o) {
         System.out.println(o.toString());
     }
+
     public static void main(String[] args) {
         launch(args);
     }
 
     Config allHosts = new Config();
+    BorderPane border = new BorderPane();
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -53,14 +56,19 @@ public class Main extends Application {
         Config hostBoard = new Config();
         hostBoard = readConfig();
 
-        BorderPane border = new BorderPane();
+
         HBox hbox = addHBox();
         border.setTop(hbox);
         border.setLeft(addVBox());
         addStackPane(hbox);         // Add stack to HBox in top region
 
-        primaryStage.setScene(new Scene(border));
-        primaryStage.setResizable(true);
+        Scene mainScene = new Scene(border, 800, 400);
+
+        File f = new File("src/SShConfig/stylesheet.css");
+        mainScene.getStylesheets().clear();
+        mainScene.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
+        primaryStage.setScene(mainScene);
+//        primaryStage.setResizable(true);
         primaryStage.show();
     }
 
@@ -111,56 +119,48 @@ public class Main extends Application {
         return hbox;
     }
 
+    public VBox detailsVBox(GridPane grid) {
+        VBox detailsBox = new VBox();
+        detailsBox.setPadding(new Insets(30, 0, 0, 0));
+        detailsBox.setSpacing(0);
+
+        ScrollPane s1 = new ScrollPane();
+        s1.setPrefSize(510, 300);
+        s1.setContent(grid);
+        detailsBox.getChildren().add(s1);
+
+        return detailsBox;
+    }
+
     public VBox addVBox() throws IOException {
         VBox vbox = new VBox();
         vbox.setPadding(new Insets(10));
         vbox.setSpacing(8);
 
-        Text title = new Text("Available Host Details");
-        title.setFont(Font.font("Arial", 14));
+        Text title = new Text("HOSTS");
+        title.setFont(Font.font("Verdana", 12));
         vbox.getChildren().add(title);
 
-        final Accordion accordion = new Accordion();
-        accordion.setPrefWidth(600);
-        TitledPane[] hostPaneList = new TitledPane[allHosts.getNumberOfHosts()];
+        //list navigation start here
+        ObservableList names = FXCollections.observableArrayList();
+        final ListView listView = new ListView(names);
+        listView.setPrefSize(200, 300);
+        listView.setEditable(true);
+        //list navigation end here
 
-        int hostCounter=0;
-
-        for (Host h: allHosts.getAllHosts()) {
-
-            GridPane grid = new GridPane();
-            grid.setVgap(4); //line spacing between grid elements
-            grid.setPadding(new Insets(5, 5, 5, 50)); //last value sets left-indentation
-            grid.add(new Label("Host "), 0, 0);
-            grid.add(new TextField(h.getHostName()), 1, 0);
-            grid.add(new Label("Port "), 0, 1);
-            grid.add(new TextField(String.valueOf(h.getPort())), 1, 1);
-            grid.add(new Label("User "), 0, 2);
-            grid.add(new TextField(h.getUser()), 1, 2);
-            Set<Integer> localForwardList = h.getLocalForward().keySet();
-
-            int addRowCount = 3; //the last index used was 2
-            for(int key: localForwardList){
-                grid.addRow(addRowCount, new Label("Local Forward "));
-                grid.add(new TextField(String.valueOf(key)), 1, addRowCount);
-                grid.add(new TextField(h.getLocalForward().get(key).toString()), 2, addRowCount);
-                addRowCount ++;
-            }
-
-            ScrollPane scrollPane = new ScrollPane();
-            scrollPane.setFitToHeight(true);
-            scrollPane.setContent(grid);
-
-            hostPaneList[hostCounter] = new TitledPane(h.getHostAlias(), scrollPane);
-
-            hostPaneList[hostCounter].setText(h.getHostAlias());
-            hostPaneList[hostCounter].setContent(scrollPane);
-            hostCounter ++;
+        for (Host h : allHosts.getAllHosts()) {
+            names.add(h.getHostAlias());
         }
-        accordion.getPanes().addAll(hostPaneList);
-        accordion.setPrefHeight(400);
-//        accordion.setExpandedPane(hostPaneList[0]);
-        vbox.getChildren().add(accordion);
+
+        listView.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<String>() {
+                    public void changed(ObservableValue<? extends String> ov,
+                                        String old_val, String new_val) {
+                        border.setCenter(detailsVBox(getHostDetails(new_val)));
+
+                    }
+                });
+        vbox.getChildren().add(listView);
 
         return vbox;
     }
@@ -190,4 +190,31 @@ public class Main extends Application {
         HBox.setHgrow(stack, Priority.ALWAYS);    // Give stack any extra space
     }
 
+    public GridPane getHostDetails(String hostAlias) {
+        GridPane grid = new GridPane();
+        for (Host h : allHosts.getAllHosts()) {
+
+            if (h.getHostAlias().equals(hostAlias)) {
+                grid.setVgap(4); //line spacing between grid elements
+                grid.setPadding(new Insets(5, 5, 5, 50)); //last value sets left-indentation
+                grid.add(new Label("Host "), 0, 0);
+                grid.add(new TextField(h.getHostName()), 1, 0);
+                grid.add(new Label("Port "), 0, 1);
+                grid.add(new TextField(String.valueOf(h.getPort())), 1, 1);
+                grid.add(new Label("User "), 0, 2);
+                grid.add(new TextField(h.getUser()), 1, 2);
+                Set<Integer> localForwardList = h.getLocalForward().keySet();
+
+                int addRowCount = 3; //the last index used was 2
+                for (int key : localForwardList) {
+                    grid.addRow(addRowCount, new Label("Local Forward "));
+                    grid.add(new TextField(String.valueOf(key)), 1, addRowCount);
+                    grid.add(new TextField(h.getLocalForward().get(key).toString()), 2, addRowCount);
+                    addRowCount++;
+                }
+            }
+        }
+        return grid;
+
+    }
 }
